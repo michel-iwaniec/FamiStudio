813ae280ad4f030750a9303b1531df6747817d81
Michel Iwaniec <michel.iwaniec@codegeneration.co.uk>
2024-04-09 23:52:59 +0200

    Remove definitions of SONG1, SONG2, SONG3, DPCM and VECTORS from build_demo_sdas6500.bat, relying only on the .org statements in demo_sdas6500.s

--- SoundEngine/DemoSource/build_demo_sdas6500.bat
+++ SoundEngine/DemoSource/build_demo_sdas6500.bat
@@ -1,5 +1,5 @@
 sdas6500 -pogn -I"." -y -s -l demo_sdas6500.o demo_sdas6500.s
-sdld6808 -n -i -j -y -w -u -w -b _ZP=0x0000 -b _OAM=0x0200 -b _CODE=0x8000 -b _SONG1=0xA000 -b _SONG2=0xC000 -b _SONG3=0xD000 -b _DPCM=0xE000 -b VECTORS=0xFFFA demo_sdas6500.ihx demo_sdas6500.o
+sdld6808 -n -i -j -y -w -u -w -b _ZP=0x0000 -b _OAM=0x0200 -b _CODE=0x8000 demo_sdas6500.ihx demo_sdas6500.o
 ihxcheck demo_sdas6500.ihx
 makebin -s 73728 -o 32752 demo_sdas6500.ihx demo_sdas6500.nes
 copy demo_sdas6500.nes ..\

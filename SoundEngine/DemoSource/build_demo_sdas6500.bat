9cf7ee291dad67a665ad757665456038eba43c7f
Michel Iwaniec <michel.iwaniec@codegeneration.co.uk>
2024-04-10 00:31:11 +0200

    * Add SDCC binaries from gbdk-2020 commit b5e9f5e to Tools folder * Update build_demo_sdas6500.bat to use binaries from Tools folder

--- SoundEngine/DemoSource/build_demo_sdas6500.bat
+++ SoundEngine/DemoSource/build_demo_sdas6500.bat
@@ -1,6 +1,6 @@
-sdas6500 -pogn -I"." -y -s -l demo_sdas6500.o demo_sdas6500.s
-sdld6808 -n -i -j -y -w -u -w -b _ZP=0x0000 -b _OAM=0x0200 -b _CODE=0x8000 demo_sdas6500.ihx demo_sdas6500.o
-ihxcheck demo_sdas6500.ihx
-makebin -s 73728 -o 32752 demo_sdas6500.ihx demo_sdas6500.nes
+..\..\Tools\sdas6500 -pogn -I"." -y -s -l demo_sdas6500.o demo_sdas6500.s
+..\..\Tools\sdld6808 -n -i -j -y -w -u -w -b _ZP=0x0000 -b _OAM=0x0200 -b _CODE=0x8000 demo_sdas6500.ihx demo_sdas6500.o
+..\..\Tools\ihxcheck demo_sdas6500.ihx
+..\..\Tools\makebin -s 73728 -o 32752 demo_sdas6500.ihx demo_sdas6500.nes
 copy demo_sdas6500.nes ..\
 del demo_sdas6500.nes *.o
